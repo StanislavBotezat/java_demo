7a2fedc1c80cc6b4e8d66196477a8c784fae9a57
STAS BOTEZAT <botezatsv@gmail.com>
2024-03-13 02:00:48 +0200

    Updated .gitignore

---
+++ src/Exercise/SimpleNumbers.java
@@ -0,0 +1,29 @@
+package Exercise;
+
+public class SimpleNumbers {
+    public static void main(String[] args) {
+int count = 0;
+        for (int i = 2; i <= 100000; i++) {
+            boolean simp = true;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    simp = false;
+                    break;
+                }
+            }
+            if (simp) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
