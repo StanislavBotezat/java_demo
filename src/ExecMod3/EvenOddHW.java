4c8525c1a279b846da0c8f76c40aaab444b20b90
STAS BOTEZAT <botezatsv@gmail.com>
2024-03-25 19:42:21 +0200

    Homework

---
+++ src/ExecMod3/EvenOddHW.java
@@ -0,0 +1,20 @@
+package ExecMod3;
+
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+
+        for (; ;) {
+            System.out.println("Ведите число: ");
+            int num = in.nextInt();
+            if (num % 2 == 0) System.out.println("Число четное");
+            else System.out.println("число нечетное");
+            System.out.println("желаете продолжить? Y/N");
+            String answer = in.next();
+            if (answer.equalsIgnoreCase("N")) break;
+        }
+    }
+}
